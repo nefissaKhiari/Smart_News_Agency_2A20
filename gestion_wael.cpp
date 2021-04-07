19ee1561134c4c2c462e4aff6a86d04d4683a461
wael <wael.belgaied@esprit.tn>
2021-04-08 00:36:01 +0100

    wael

---
+++ gestion_wael.cpp
@@ -0,0 +1,293 @@
+#include "gestion_wael.h"
+#include "ui_gestion_wael.h"
+#include <QtMultimedia/QMediaPlayer>
+
+gestion_wael::gestion_wael(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::gestion_wael)
+{
+    ui->setupUi(this);
+
+    //affichage contenu base
+    show_tables();
+}
+
+gestion_wael::~gestion_wael()
+{
+    delete ui;
+}
+
+void gestion_wael::show_tables()
+{
+    show_equipement();
+    show_maintenance();
+}
+
+/*********************************** equipement ****************************/
+
+//************************ crud
+
+//ajout
+void gestion_wael::on_actionadd_equipement_triggered()
+{
+    //creation instance
+        add_equipement ac(this);
+
+        //ouvrir dialog
+        int res=ac.exec();
+        if (res == QDialog::Rejected )
+            return;
+
+      //recuperation des donnees
+        QString s1=ac.id();
+        QString s2=ac.nom();
+        QString s3=ac.date();
+        QString s4=ac.etat();
+        double x=ac.prix();
+
+        //ajout
+        equipement mc(s1,s2,s3,x,s4);
+        mc.ajouter();
+
+        //refresh du tableau (affichage)
+          show_equipement();
+}
+
+//selection
+void gestion_wael::on_table_equipement_clicked(const QModelIndex &index)
+{
+    selected_equipement=ui->table_equipement->model()->data(index).toString();
+}
+
+//modification
+void gestion_wael::on_table_equipement_doubleClicked(const QModelIndex &index)
+{
+    add_equipement ac(this);
+
+  ac.fill_form(selected_equipement);
+  int res=ac.exec();
+  if (res == QDialog::Rejected )
+    return;
+
+
+  //recuperation des donnees
+  QString s2=ac.nom();
+  QString s3=ac.date();
+  QString s4=ac.etat();
+  double x=ac.prix();
+
+    //mofication
+    equipement mc(selected_equipement,s2,s3,x,s4);
+    mc.modifier(selected_equipement);
+
+    //refresh du tableau (affichage)
+      show_equipement();
+}
+
+//suppression
+void gestion_wael::on_actiondelete_equipement_triggered()
+{
+    equipement mc;
+  mc.supprimer(selected_equipement);
+
+ //refresh du tableau (affichage)
+   show_equipement();
+}
+
+//affichage
+void gestion_wael::show_equipement()
+{
+    //creation model (masque du tableau) : permet recherche et tri
+        proxy_equipement = new QSortFilterProxyModel();
+
+     //definir la source (tableau original)
+        proxy_equipement->setSourceModel(tmp_equipement.afficher());
+
+     //pour la recherche
+        proxy_equipement->setFilterCaseSensitivity(Qt::CaseInsensitive); // S=s (pas de difference entre majiscule et miniscule)
+
+        //remplissage tableau avec le masque
+        ui->table_equipement->setModel(proxy_equipement);
+}
+
+//************************ metiers
+
+
+//recherche dynamique
+void gestion_wael::on_equipement_sel_col_currentIndexChanged(const QString &arg1)
+{
+    sel_col_equipement=ui->equipement_sel_col->currentIndex()-1;
+    proxy_equipement->setFilterKeyColumn(sel_col_equipement); // chercher dans tout le tableau (-1) ou donner le numero de la colone
+}
+void gestion_wael::on_rech_equipement_textChanged(const QString &arg1)
+{
+    proxy_equipement->setFilterFixedString(arg1);
+}
+
+
+/*********************************** maintenance ****************************/
+
+//************************ crud
+
+//ajout
+
+void gestion_wael::on_actionadd_maintenance_triggered()
+{
+    //creation instance
+        add_maintenance ac(this);
+
+        //ouvrir dialog
+        int res=ac.exec();
+        if (res == QDialog::Rejected )
+            return;
+
+      //recuperation des donnees
+        QString s1=ac.id();
+        QString s2=ac.equipement();
+        QString s3=ac.duree();
+        QString s4=ac.panne();
+        double x=ac.total();
+
+
+        //ajout
+        maintenance mc(s1,s2,s3,x,s4);
+        mc.ajouter();
+
+        //refresh du tableau (affichage)
+          show_maintenance();
+}
+
+//selection
+void gestion_wael::on_table_maintenance_clicked(const QModelIndex &index)
+{
+    selected_maintenance=ui->table_maintenance->model()->data(index).toString();
+}
+
+//modification
+void gestion_wael::on_table_maintenance_doubleClicked(const QModelIndex &index)
+{
+    add_maintenance ac(this);
+
+  ac.fill_form(selected_maintenance);
+  int res=ac.exec();
+  if (res == QDialog::Rejected )
+    return;
+
+
+  //recuperation des donnees
+  QString s2=ac.equipement();
+  QString s3=ac.duree();
+  QString s4=ac.panne();
+  double x=ac.total();
+
+    //mofication
+    maintenance mc(selected_maintenance,s2,s3,x,s4);
+    mc.modifier(selected_maintenance);
+
+    //refresh du tableau (affichage)
+    show_maintenance();
+
+}
+
+//suppression
+void gestion_wael::on_actiondelete_maintenance_triggered()
+{
+    maintenance mc;
+  mc.supprimer(selected_maintenance);
+
+ //refresh du tableau (affichage)
+  show_maintenance();
+}
+
+//affichage
+void gestion_wael::show_maintenance()
+{
+    //creation model (masque du tableau) : permet recherche et tri
+        proxy_maintenance = new QSortFilterProxyModel();
+
+     //definir la source (tableau original)
+        proxy_maintenance->setSourceModel(tmp_maintenance.afficher());
+
+     //pour la recherche
+        proxy_maintenance->setFilterCaseSensitivity(Qt::CaseInsensitive); // S=s (pas de difference entre majiscule et miniscule)
+
+        //remplissage tableau avec le masque
+        ui->table_maintenance->setModel(proxy_maintenance);
+}
+
+//************************ metiers
+
+//recherche dynamique
+void gestion_wael::on_maintenance_sel_col_currentIndexChanged(const QString &arg1)
+{
+    sel_col_maintenance=ui->maintenance_sel_col->currentIndex()-1;
+    proxy_maintenance->setFilterKeyColumn(sel_col_maintenance); // chercher dans tout le tableau (-1) ou donner le numero de la colone
+
+}
+void gestion_wael::on_rech_maintenance_textChanged(const QString &arg1)
+{
+    proxy_maintenance->setFilterFixedString(arg1);
+}
+
+//imprimer
+void gestion_wael::on_pushButton_2_clicked()
+{
+    //add sound effect
+
+        QMediaPlayer *player = new QMediaPlayer;
+        player->setMedia(QUrl::fromLocalFile("C:/Users/Seif/Desktop/wael_bel_gaied/click.wav"));
+        player->setVolume(50);
+        player->play();
+
+
+    //end sound effect
+
+
+        QString strStream;
+        QTextStream out(&strStream);
+
+        const int rowCount = ui->table_maintenance->model()->rowCount();
+        const int columnCount =ui->table_maintenance->model()->columnCount();
+
+        out <<  "<html>\n"
+            "<head>\n"
+            "<meta Content=\"Text/html; charset=Windows-1251\">\n"
+            <<  QString("<title>%1</title>\n").arg("strTitle")
+            <<  "</head>\n"
+            "<body bgcolor=#ffffff link=#5000A0>\n"
+            "<table border=1 cellspacing=0 cellpadding=2>\n";
+
+        // headers
+        out << "<thead><tr bgcolor=#f0f0f0>";
+        for (int column = 0; column < columnCount; column++)
+            if (ui->table_maintenance->isColumnHidden(column))
+                out << QString("<th>%1</th>").arg(ui->table_maintenance->model()->headerData(column, Qt::Horizontal).toString());
+        out << "</tr></thead>\n";
+
+        // data table
+        for (int row = 0; row < rowCount; row++) {
+            out << "<tr>";
+            for (int column = 0; column < columnCount; column++) {
+                if (!ui->table_maintenance->isColumnHidden(column)) {
+                    QString data = ui->table_maintenance->model()->data(ui->table_maintenance->model()->index(row, column)).toString().simplified();
+                    out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
+                }
+            }
+            out << "</tr>\n";
+        }
+        out <<  "</table>\n"
+            "</body>\n"
+            "</html>\n";
+
+        QTextDocument *document = new QTextDocument();
+        document->setHtml(strStream);
+
+        QPrinter printer;
+
+        QPrintDialog *dialog = new QPrintDialog(&printer, NULL);
+        if (dialog->exec() == QDialog::Accepted) {
+            document->print(&printer);
+        }
+
+        delete document;
+}
---
+++ wael_bel_gaied.pro
@@ -0,0 +1,51 @@
+QT       += core gui sql printsupport multimedia
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+# The following define makes your compiler emit warnings if you use
+# any Qt feature that has been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    add_equipement.cpp \
+    add_maintenance.cpp \
+    connexion.cpp \
+    equipement.cpp \
+    gestion_wael.cpp \
+    main.cpp \
+    maintenance.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    add_equipement.h \
+    add_maintenance.h \
+    connexion.h \
+    equipement.h \
+    gestion_wael.h \
+    maintenance.h \
+    mainwindow.h
+
+FORMS += \
+    add_equipement.ui \
+    add_maintenance.ui \
+    gestion_wael.ui \
+    mainwindow.ui
+
+RC_ICONS= earlypost.ico
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    Resources.qrc
